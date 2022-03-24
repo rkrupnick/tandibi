cf3fd5b435c919a7aeb51f852ce07261901541be
rkrupnick <rachel@rachelkrupnick.com>
2022-03-24 02:38:36 -0400

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(
+    first_name: "Sam",
+    last_name: "Shepard",
+    email: "sam@example.org",
+    username: "samiam"
+)
+
+user2 = User.create!(
+    first_name: "Peter",
+    last_name: "Potter",
+    email: "pete@example.org",
+    username: "petesneat"
+)
+
+Bond.create(user: user1, friend: user2, state: Bond::FOLLOWING)
+Bond.create(user: user2, friend: user1, state: Bond::FOLLOWING)
+place = Place.create!(
+locale: "en",
+name: "Hotel Majapahit",
+place_type: "hotel",
+coordinate: "POINT (112.739898 -7.259836 0)"
+)
+post = Post.create!(user: user1, postable: Status.new(
+text: "Whohoo! I am in Surabaya!!!!"
+))
+Post.create!(user: user2, postable: Status.new(
+text: "Wow! Looks great! Have fun, Sam!"
+), thread: post)
+Post.create!(user: user1, postable: Status.new(
+text: "Ya! Ya! Ya! Are you in town?"
+), thread: post)
+Post.create!(user: user2, postable: Status.new(
+text: "Yups! Let's explore the city!"
+), thread: post)
+Post.create(user: user1, postable: Sight.new(
+place: place, activity_type: Sight::CHECKIN
+))
