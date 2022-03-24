1e214f274984859c13a78364a7725a40cead360e
rkrupnick <rachel@rachelkrupnick.com>
2022-03-24 00:30:16 -0400

    Add spec for bond persistence

--- spec/models/bond_spec.rb
+++ spec/models/bond_spec.rb
@@ -23,8 +23,12 @@ require 'rails_helper'
 RSpec.describe Bond, type: :model do
   describe "#valid?" do
     it "should validate the state correctly" do
-      friend = User.new
-      user = User.new
+      user = User.create email: "e1@example.org",
+        first_name: "Edwin",
+        username: "e1"
+      friend = User.create email: "a1@example.org",
+       first_name: "Adam",
+        username: "a1"
       bond = Bond.new(
         user_id: user.id,
         friend_id: friend.id
@@ -35,5 +39,28 @@ RSpec.describe Bond, type: :model do
         expect(bond).to be_valid
       end
     end
+  end
+
+  describe "#save" do
+    context "when complete data is given" do
+      it "can be persisted" do
+        user = User.create email: "e1@example.org",
+          first_name: "Edwin",
+          username: "e1"
+        friend = User.create email: "a1@example.org",
+          first_name: "Adam",
+          username: "a1"
+        bond = Bond.new(
+          user: user,
+          friend: friend,
+          state: Bond::REQUESTING
+        )
+        bond.save
+        expect(bond).to be_persisted
+        expect(bond.user).to eq user
+        expect(bond.friend).to eq friend
+      end
+    end
+    
   end
 end
