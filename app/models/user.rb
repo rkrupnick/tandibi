d958b7bb3903d37ed4589d49c3d8a0ff2de5ae63
rkrupnick <rachel@rachelkrupnick.com>
2022-03-28 01:31:42 -0400

    Update password views

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         authentication_keys: [:login]
+         authentication_keys: [:login],
+         reset_password_keys: ["login"]
     
     attr_writer :login
     validates_uniqueness_of :email
@@ -57,6 +58,25 @@ class User < ApplicationRecord
         conditions = conditions.dup
         login = conditions.delete(:login).downcase
         find_authenticatable(login)
+    end
+
+    def self.send_reset_password_instructions(conditions)
+        recoverable = find_recoverable_or_init_with_errors(conditions)
+        if recoverable.persisted?
+            recoverable.send_reset_password_instructions
+        end
+        recoverable
+    end
+
+    def self.find_recoverable_or_init_with_errors(conditions)
+        conditions = conditions.dup
+        login = conditions.delete(:login).downcase
+        recoverable = find_authenticatable(login)
+        unless recoverable
+            recoverable = new(login: login)
+            recoverable.errors.add(:login, login.present? ? :not_found : :blank)
+        end
+        recoverable
     end
 
     def login
