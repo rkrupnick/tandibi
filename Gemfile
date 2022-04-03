9f0489c4b64e630dc7a8727c260866e622592b2a
rkrupnick <rachel@rachelkrupnick.com>
2022-04-02 20:33:54 -0400

    Update email style

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'devise', '~> 4.7'
 gem 'evil-icons'
+gem 'premailer-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,9 @@ module Tandibi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.to_prepare do
+      Devise::Mailer.layout "mailer"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
